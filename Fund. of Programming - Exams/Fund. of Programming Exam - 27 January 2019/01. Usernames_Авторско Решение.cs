7d44e4374a55310d4f2ca2aeccaab191de60fd55
Radina Avramova <99686592+RadinaAvramova@users.noreply.github.com>
2024-05-19 19:20:50 +0300

    Add files via upload

---
+++ Fund. of Programming - Exams/Fund. of Programming Exam - 27 January 2019/01. Usernames_Авторско Решение.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ASCII Approach:
+            while (true)
+            {
+                string username = Console.ReadLine();
+                bool checkifInvalid = false;
+
+                for (int j = 0; j < username.Length; j++)
+                {
+                    if (username[j] < 65 || (username[j] > 90 && username[j] < 97) || username[j] > 122)
+                    {
+                        checkifInvalid = true;
+                    }
+                }
+
+                if (checkifInvalid)
+                {
+                    Console.WriteLine("Invalid username!");
+                }
+                else
+                {
+                    break;
+                }
+            }
+            //REGEX Approach:
+            //string name = string.Empty;
+            //while (true)
+            //{
+            //    name = Console.ReadLine();
+
+            //    var regex = new Regex("^[A-Za-z]+$");
+
+            //    Match match = regex.Match(name);
+
+            //    if (match.Success)
+            //    {
+            //        break;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Invalid username!");
+            //    }
+            //}
+        }
+    }
+}
---
+++ Fund. of Programming - Exams/Fund. of Programming Exam - 27 January 2019/04. Car_Авторско Решение.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Card
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            var cars = new List<Car>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split().ToList();
+                string model = input[0];
+                int fuelAmount = int.Parse(input[1]);
+                double fuelConsumationPer1Km = double.Parse(input[2]);
+
+                if (cars.Any(x => x.Model == model))
+                {
+                    continue;
+                }
+
+                Car car = new Car(model, fuelAmount, fuelConsumationPer1Km);
+
+                cars.Add(car);
+            }
+            var command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "End")
+            {
+                var token = command.Split().ToList();
+                var carModel = token[1];
+                var amountOfKm = int.Parse(token[2]);
+
+                var equalCar = cars.FirstOrDefault(x => x.Model == carModel);
+
+                if (equalCar != null)
+                {
+                    if (amountOfKm * equalCar.FuelConsumptionFor1km <= equalCar.FuelAmount)
+                    {
+                        equalCar.FuelAmount -= (amountOfKm * equalCar.FuelConsumptionFor1km);
+                        equalCar.Distance += amountOfKm;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Insufficient fuel for the drive");
+                    }
+                }
+
+            }
+
+            foreach (var c in cars)
+            {
+                Console.WriteLine(c);
+            }
+        }
+    }
+
+    public class Car
+    {
+        public Car(string model, double fuelAmount, double fuelConsumationFor1Km)
+        {
+            this.Model = model;
+            this.FuelAmount = fuelAmount;
+            this.FuelConsumptionFor1km = fuelConsumationFor1Km;
+        }
+
+        public string Model { get; set; }
+        public double FuelAmount { get; set; }
+        public double FuelConsumptionFor1km { get; set; }
+        public int Distance { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Model} {this.FuelAmount:f2} {this.Distance}";
+        }
+    }
+}
